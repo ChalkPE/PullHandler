16719a8185f10e8129b2e23d07e119569897d718
ChalkPE <amato0617@gmail.com>
2015-09-09 17:24:17 +0900

    Initial commit

---
+++ src/PullHandler.java
@@ -0,0 +1,65 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author ChalkPE <chalkpe@gmail.com>
+ * @since 2015-09-09
+ */
+public class PullHandler {
+    public static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if(args.length < 1){
+            System.err.println("java PullHandler <git-repo>");
+            return;
+        }
+
+        final Path path = Paths.get(args[0]);
+        System.out.println("Working on: " + path.toAbsolutePath());
+
+        if(Files.notExists(path) || !Files.isDirectory(path)){
+            throw new FileNotFoundException();
+        }
+
+        try(final WatchService watcher = FileSystems.getDefault().newWatchService()){
+            path.register(watcher, StandardWatchEventKinds.ENTRY_CREATE);
+
+            while(true){
+                final WatchKey key = watcher.take();
+
+                for(WatchEvent<?> watchEvent : key.pollEvents()){
+                    final WatchEvent.Kind<?> kind = watchEvent.kind();
+
+                    if(kind == StandardWatchEventKinds.OVERFLOW) continue;
+
+                    final WatchEvent<Path> pathWatcher = (WatchEvent<Path>) watchEvent;
+                    final Path filename = pathWatcher.context();
+
+                    final Path pullPlease = path.resolve(filename);
+
+                    if(filename.toString().equals("PULL_PLEASE")){
+                        final Path pullResult = path.resolve("PULL_RESULT");
+
+                        Files.move(pullPlease, pullResult, StandardCopyOption.REPLACE_EXISTING);
+                        Files.copy(new ProcessBuilder("git", "pull").directory(path.toFile()).start().getInputStream(), pullResult, StandardCopyOption.REPLACE_EXISTING);
+
+                        System.out.println("Pulled: " + DATE_FORMAT.format(new Date()));
+                    }
+                }
+
+                if(!key.reset()) break;
+            }
+        }
+    }
+}
